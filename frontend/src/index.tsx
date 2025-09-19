bc16aadf2efe4fb8a533c48328cd043da82d1386
Joe M. <chefjoemiller1992@gmail.com>
2025-09-19 23:40:32 +0100

    Tsx still buggy, setting up basic views and bp for sim data

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,1 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
